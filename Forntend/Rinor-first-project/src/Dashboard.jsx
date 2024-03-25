e64f16e21b8afea626626a03f493da1ff90a1afb
FarrukhHGithub <BussinessGuy5909@gmail.com>
2024-03-25 23:36:05 +0500

    add dashboard sample chart

--- Forntend/Rinor-first-project/src/Dashboard.jsx
+++ Forntend/Rinor-first-project/src/Dashboard.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React from 'react';
+import {
+    Box,
+    Card,
+    CardContent,
+    Typography
+} from '@mui/material';
 
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+import {
+    Chart as ChartJS,
+    LinearScale,
+    CategoryScale,
+    BarElement,
+    PointElement,
+    LineElement,
+    Legend,
+    Tooltip,
+    LineController,
+    BarController,
+} from 'chart.js';
+import { Chart } from 'react-chartjs-2';
+
+ChartJS.register(
+    LinearScale,
+    CategoryScale,
+    BarElement,
+    PointElement,
+    LineElement,
+    Legend,
+    Tooltip,
+    LineController,
+    BarController
+);
+
+const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top',
+        },
+    },
+};
+
+const labels = ['Jan 01', "Jan 03", "Jan 05", "Jan 07", "Jan 09", "Jan 11"];
+
+const data = {
+    labels,
+    datasets: [
+        {
+            type: 'line',
+            label: 'AP',
+            borderColor: 'rgb(255, 99, 132)',
+            borderWidth: 2,
+            fill: false,
+            data: [50, 54, 74, 63, 70, 76, 63, 70, 76, 63, 70, 76],
+        },
+        {
+            type: 'bar',
+            label: 'AR',
+            backgroundColor: 'rgb(75, 192, 192)',
+            data: [67, 54, 80, 81, 56, 55, 40, 30, 20, 10, 75, 14],
+            borderColor: 'white',
+            borderWidth: 2,
+        },
+    ],
+};
+
+function Dashboard() {
+    return (
+        <>
+            <Box mt={3}>
+                <Card style={{ backgroundColor: "#f9faff" }}>
+                    <CardContent>
+                        <Typography mb={5} variant='h5'>
+                            AP and AR Balance
+                        </Typography>
+
+                        <Chart type='bar' data={data} options={options} />
+                    </CardContent>
+                </Card>
+            </Box>
+        </>
+    )
 }
 
-export default Dashboard
+export default Dashboard;
